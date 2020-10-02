3f59ad165a33c43fbd7a7b41b4779178a77042b2
jendos-kgd <edanileyko@gmail.com>
2020-10-02 11:29:10 +0200

    made airdatepicker on two inputs

--- assets/includes/datepicker/airdatepicker.js
+++ assets/includes/datepicker/airdatepicker.js
@@ -1,19 +1,21 @@
 
 $('.datepicker-here').datepicker({
-  prevHtml: '<svg><path d="M 20,9 l -8,8 l 8,8 M 12, 17 h 16"></path></svg>',
-  nextHtml: '<svg><path d="M 12,9 l 8,8 l -8,8 M 19, 17 h -16"></path></svg>',
+  prevHtml: '<svg><path d="M 20,9 l -8,8 l 8,8 M 12, 17 h 16"></path></svg>',  // Рисуем правую стрелку
+  nextHtml: '<svg><path d="M 12,9 l 8,8 l -8,8 M 19, 17 h -16"></path></svg>', // Рисуем левую стрелку
   navTitles: {
-    days: 'MM <i>yyyy</i>',
+    days: 'MM <i>yyyy</i>', // Убрали запятую после месяца в заголовке календаря
     months: 'yyyy',
     years: 'yyyy1 - yyyy2'
   },
   range: 'true',
-  clearButton: 'true',
-  todayButton: 'true',
-  language: {
-    today: 'Применить',
-  },
-
+  clearButton: 'true',      // Добавили кнопку Очистить
+  onSelect: (formattedDate) => {       //разбиваем даты заезда/выезда на 2 инпута
+        if (formattedDate.length > 1) { //если в массиве дат больше 1 даты
+          $('#startDate').val(formattedDate.split(",")[0]);  //в первый инпут заносим первую дату
+          $('#endDate').val(formattedDate.split(",")[1]);    //во второй инпут - вторую дату
+          this.selectedDates = formattedDate;                
+        }
+      }
 })
 
 // Добавляем фон к началу и концу диапазона дат, чтобы не было белых пробелов в диапазоне.
@@ -22,4 +24,8 @@ $('.datepicker').click(function () {
   $('.-range-to-').prepend('<div class="add-bg-range-to"></div>');
 })
 
-// Замена кнопки Сегодня на кнопку Применить
+// Добавляем кнопу Применить, которая скрывает календарь
+let okButton = '<span class="datepicker--button" data-action="hide">Применить</span>';  //Создаем кнопку Применить c data-action="hide"(метод hide прописан в исходниках) со стандартными классами кнопок air-datepicker
+$('.datepicker--button[data-action="clear"]').each(function() {  // ищем каждую кнопку Очистить в документе
+  $(okButton).insertAfter($(this));   // Вставялем кнопку Применить после кнопки очистить
+});
